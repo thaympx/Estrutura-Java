02c21b5bb20e43118252523b0989bca42b30369b
Thaline <thalinep12@gmail.com>
2024-06-11 14:39:33 -0300

    laços commit

---
+++ src/LacosNumericos.java
@@ -0,0 +1,19 @@
+public class LacosNumericos {
+    public static void main(String[] args) {
+        
+ //laços de repetição 
+
+for(int i = 1; i <= 10; i++){
+
+    for(int j = 1; j<= 10; j++) {
+        
+        System.out.println(j + "x" + i + " = " + j * i);
+    }
+// (i+=10) para ir de 10 em 10
+  // (i++) para ir de 1 em 1
+
+}
+
+    
+    }
+}
